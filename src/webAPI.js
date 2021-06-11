c5833d7a3d436c9207400de10ef0fa35fcab99d5
Jim Charnesky <mijnesky@hotmail.com>
2021-06-11 17:00:52 -0600

    Add fetch for all customers

--- src/webAPI.js
+++ src/webAPI.js
@@ -1,5 +1,14 @@
 // api file
+const fetchAllCustomerData = {
+    reutrn fetch('http://localhost:3001/api/v1/customers')
+    .then(response => response.json())
+    .catch(err => console.log("error"))
+}
 
-fetch('http://localhost:3001/api/v1/customers')
-  .then(response => response.json())
-  .catch(err => console.log("error"))
+const fetchAllRoomsData = {
+
+}
+
+const fetchAllBookingsData = {
+  
+}

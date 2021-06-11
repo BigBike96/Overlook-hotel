e9a9e4ebdc58fbdab7bd2f0623ecda238d2e5227
Jim Charnesky <mijnesky@hotmail.com>
2021-06-11 17:20:58 -0600

    Create getData function to hold Promise.all

--- src/webAPI.js
+++ src/webAPI.js
@@ -1,14 +1,22 @@
 // api file
 const fetchAllCustomerData = {
-    reutrn fetch('http://localhost:3001/api/v1/customers')
-    .then(response => response.json())
-    .catch(err => console.log("error"))
+  reutrn fetch('http://localhost:3001/api/v1/customers')
+  .then(response => response.json())
+  .catch(err => console.log("error"));
 }
 
 const fetchAllRoomsData = {
-
+  return fetch('http://localhost:3001/api/v1/rooms')
+  .then(response => response.json())
+  .catch(err => console.log("error"));
 }
 
 const fetchAllBookingsData = {
-  
+  return fetch('http://localhost:3001/api/v1/bookings')
+  .then(response => response.json())
+  .catch(err => console.log("error"));
 }
+const getData = () => {
+  return Promise.all([ fetchAllCustomerData(), fetchAllRoomsData(), fetchAllBookingsData() ])
+}
+export { fetchAllCustomerData, fetchAllRoomsData, fetchAllBookingsData }

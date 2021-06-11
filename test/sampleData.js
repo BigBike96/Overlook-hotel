06df2339e7bffa4db1156136fcb8be90ec81e519
Jim Charnesky <mijnesky@hotmail.com>
2021-06-11 16:42:57 -0600

    Add sample bookings data

--- test/sampleData.js
+++ test/sampleData.js
@@ -6,204 +6,44 @@ const customerSampleData = {
 const allCustomerData = {
   "customers": [
     {
-    "id": 1,
-    "name": "Leatha Ullrich"
+      "id": 1,
+      "name": "Leatha Ullrich"
     },
     {
-    "id": 2,
-    "name": "Rocio Schuster"
+      "id": 2,
+      "name": "Rocio Schuster"
     },
     {
-    "id": 3,
-    "name": "Kelvin Schiller"
+      "id": 3,
+      "name": "Kelvin Schiller"
     },
     {
-    "id": 4,
-    "name": "Kennedi Emard"
+      "id": 4,
+      "name": "Kennedi Emard"
     },
     {
-    "id": 5,
-    "name": "Rhiannon Little"
+      "id": 5,
+      "name": "Rhiannon Little"
     },
     {
-    "id": 6,
-    "name": "Fleta Schuppe"
+      "id": 6,
+      "name": "Fleta Schuppe"
     },
     {
-    "id": 7,
-    "name": "Dell Rath"
+      "id": 7,
+      "name": "Dell Rath"
     },
     {
-    "id": 8,
-    "name": "Era Hand"
+      "id": 8,
+      "name": "Era Hand"
     },
     {
-    "id": 9,
-    "name": "Faustino Quitzon"
+      "id": 9,
+      "name": "Faustino Quitzon"
     },
     {
-    "id": 10,
-    "name": "Tony Armstrong"
-    },
-    {
-    "id": 11,
-    "name": "Melissa Sauer"
-    },
-    {
-    "id": 12,
-    "name": "Kari Keeling"
-    },
-    {
-    "id": 13,
-    "name": "Christina Kulas"
-    },
-    {
-    "id": 14,
-    "name": "Dallas Schultz"
-    },
-    {
-    "id": 15,
-    "name": "Maria Lakin"
-    },
-    {
-    "id": 16,
-    "name": "Garry Mills"
-    },
-    {
-    "id": 17,
-    "name": "Trudie Grimes"
-    },
-    {
-    "id": 18,
-    "name": "Triston Leffler"
-    },
-    {
-    "id": 19,
-    "name": "Leatha Hettinger"
-    },
-    {
-    "id": 20,
-    "name": "Keon Kirlin"
-    },
-    {
-    "id": 21,
-    "name": "Kelsie Rath"
-    },
-    {
-    "id": 22,
-    "name": "Amalia Rowe"
-    },
-    {
-    "id": 23,
-    "name": "Angus Swift"
-    },
-    {
-    "id": 24,
-    "name": "Salvatore Marquardt"
-    },
-    {
-    "id": 25,
-    "name": "Rashawn Langworth"
-    },
-    {
-    "id": 26,
-    "name": "Antone Olson"
-    },
-    {
-    "id": 27,
-    "name": "Sigrid Barrows"
-    },
-    {
-    "id": 28,
-    "name": "Isaiah Daniel"
-    },
-    {
-    "id": 29,
-    "name": "Maybelle Raynor"
-    },
-    {
-    "id": 30,
-    "name": "Devan Mante"
-    },
-    {
-    "id": 31,
-    "name": "Deja Bradtke"
-    },
-    {
-    "id": 32,
-    "name": "Euna Balistreri"
-    },
-    {
-    "id": 33,
-    "name": "Rae Wisozk"
-    },
-    {
-    "id": 34,
-    "name": "Ardella Jakubowski"
-    },
-    {
-    "id": 35,
-    "name": "Deion Hartmann"
-    },
-    {
-    "id": 36,
-    "name": "Michel Kunze"
-    },
-    {
-    "id": 37,
-    "name": "Gilda Abernathy"
-    },
-    {
-    "id": 38,
-    "name": "Alessia Rippin"
-    },
-    {
-    "id": 39,
-    "name": "Moises Nader"
-    },
-    {
-    "id": 40,
-    "name": "Leta Lakin"
-    },
-    {
-    "id": 41,
-    "name": "Francisca Trantow"
-    },
-    {
-    "id": 42,
-    "name": "Freda Bednar"
-    },
-    {
-    "id": 43,
-    "name": "Earline Hamill"
-    },
-    {
-    "id": 44,
-    "name": "Ruth Ebert"
-    },
-    {
-    "id": 45,
-    "name": "Chauncey Dooley"
-    },
-    {
-    "id": 46,
-    "name": "Louisa Homenick"
-    },
-    {
-    "id": 47,
-    "name": "Rachael Schinner"
-    },
-    {
-    "id": 48,
-    "name": "Kaylee Hermann"
-    },
-    {
-    "id": 49,
-    "name": "Arnulfo Simonis"
-    },
-    {
-    "id": 50,
-    "name": "Eldridge Muller"
+      "id": 10,
+      "name": "Tony Armstrong"
     }
   ]
 };
@@ -211,209 +51,171 @@ const allCustomerData = {
 const allRoomsSampleData = {
   "rooms": [
     {
-    "number": 1,
-    "roomType": "residential suite",
-    "bidet": true,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 358.4
+      "number": 1,
+      "roomType": "residential suite",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 358.4
     },
     {
-    "number": 2,
-    "roomType": "suite",
-    "bidet": false,
-    "bedSize": "full",
-    "numBeds": 2,
-    "costPerNight": 477.38
+      "number": 2,
+      "roomType": "suite",
+      "bidet": false,
+      "bedSize": "full",
+      "numBeds": 2,
+      "costPerNight": 477.38
     },
     {
-    "number": 3,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "king",
-    "numBeds": 1,
-    "costPerNight": 491.14
+      "number": 3,
+      "roomType": "single room",
+      "bidet": false,
+      "bedSize": "king",
+      "numBeds": 1,
+      "costPerNight": 491.14
     },
     {
-    "number": 4,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 429.44
+      "number": 4,
+      "roomType": "single room",
+      "bidet": false,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 429.44
     },
     {
-    "number": 5,
-    "roomType": "single room",
-    "bidet": true,
-    "bedSize": "queen",
-    "numBeds": 2,
-    "costPerNight": 340.17
+      "number": 5,
+      "roomType": "single room",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 2,
+      "costPerNight": 340.17
     },
     {
-    "number": 6,
-    "roomType": "junior suite",
-    "bidet": true,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 397.02
+      "number": 6,
+      "roomType": "junior suite",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 397.02
     },
     {
-    "number": 7,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "queen",
-    "numBeds": 2,
-    "costPerNight": 231.46
+      "number": 7,
+      "roomType": "single room",
+      "bidet": false,
+      "bedSize": "queen",
+      "numBeds": 2,
+      "costPerNight": 231.46
     },
     {
-    "number": 8,
-    "roomType": "junior suite",
-    "bidet": false,
-    "bedSize": "king",
-    "numBeds": 1,
-    "costPerNight": 261.26
+      "number": 8,
+      "roomType": "junior suite",
+      "bidet": false,
+      "bedSize": "king",
+      "numBeds": 1,
+      "costPerNight": 261.26
     },
     {
-    "number": 9,
-    "roomType": "single room",
-    "bidet": true,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 200.39
-    },
-    {
-    "number": 10,
-    "roomType": "suite",
-    "bidet": false,
-    "bedSize": "twin",
-    "numBeds": 1,
-    "costPerNight": 497.64
-    },
-    {
-    "number": 11,
-    "roomType": "single room",
-    "bidet": true,
-    "bedSize": "twin",
-    "numBeds": 2,
-    "costPerNight": 207.24
-    },
-    {
-    "number": 12,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "twin",
-    "numBeds": 2,
-    "costPerNight": 172.09
-    },
-    {
-    "number": 13,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "queen",
-    "numBeds": 2,
-    "costPerNight": 423.92
-    },
-    {
-    "number": 14,
-    "roomType": "residential suite",
-    "bidet": false,
-    "bedSize": "twin",
-    "numBeds": 1,
-    "costPerNight": 457.88
-    },
-    {
-    "number": 15,
-    "roomType": "residential suite",
-    "bidet": false,
-    "bedSize": "full",
-    "numBeds": 1,
-    "costPerNight": 294.56
-    },
-    {
-    "number": 16,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "full",
-    "numBeds": 2,
-    "costPerNight": 325.6
-    },
-    {
-    "number": 17,
-    "roomType": "junior suite",
-    "bidet": false,
-    "bedSize": "twin",
-    "numBeds": 2,
-    "costPerNight": 328.15
-    },
-    {
-    "number": 18,
-    "roomType": "junior suite",
-    "bidet": false,
-    "bedSize": "king",
-    "numBeds": 2,
-    "costPerNight": 496.41
-    },
-    {
-    "number": 19,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 374.67
-    },
-    {
-    "number": 20,
-    "roomType": "residential suite",
-    "bidet": false,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 343.95
-    },
-    {
-    "number": 21,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "full",
-    "numBeds": 2,
-    "costPerNight": 429.32
-    },
-    {
-    "number": 22,
-    "roomType": "single room",
-    "bidet": false,
-    "bedSize": "full",
-    "numBeds": 2,
-    "costPerNight": 350.31
-    },
-    {
-    "number": 23,
-    "roomType": "residential suite",
-    "bidet": false,
-    "bedSize": "queen",
-    "numBeds": 2,
-    "costPerNight": 176.36
-    },
-    {
-    "number": 24,
-    "roomType": "suite",
-    "bidet": false,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 327.24
-    },
-    {
-    "number": 25,
-    "roomType": "single room",
-    "bidet": true,
-    "bedSize": "queen",
-    "numBeds": 1,
-    "costPerNight": 305.85
+      "number": 9,
+      "roomType": "single room",
+      "bidet": true,
+      "bedSize": "queen",
+      "numBeds": 1,
+      "costPerNight": 200.39
     }
   ]
 };
 
-export { customerSampleData, allCustomerData, allRoomsSampleData }
+const allBookingsSampleData = {
+  "bookings": [
+    {
+      "id": "5fwrgu4i7k55hl6sz",
+      "userID": 9,
+      "date": "2020/04/22",
+      "roomNumber": 15,
+      "roomServiceCharges": []
+    },
+    {
+      "id": "5fwrgu4i7k55hl6t5",
+      "userID": 43,
+      "date": "2020/01/24",
+      "roomNumber": 24,
+      "roomServiceCharges": []
+    },
+    {
+      "id": "5fwrgu4i7k55hl6t6",
+      "userID": 13,
+      "date": "2020/01/10",
+      "roomNumber": 12,
+      "roomServiceCharges": []
+    },
+    {
+      "id": "5fwrgu4i7k55hl6t7",
+      "userID": 20,
+      "date": "2020/02/16",
+      "roomNumber": 7,
+      "roomServiceCharges": []
+      },
+    {
+      "id": "5fwrgu4i7k55hl6t8",
+      "userID": 1,
+      "date": "2020/02/05",
+      "roomNumber": 12,
+      "roomServiceCharges": []
+      },
+    {
+      "id": "5fwrgu4i7k55hl6t9",
+      "userID": 38,
+      "date": "2020/02/14",
+      "roomNumber": 14,
+      "roomServiceCharges": []
+      },
+    {
+      "id": "5fwrgu4i7k55hl6ta",
+      "userID": 25,
+      "date": "2020/01/11",
+      "roomNumber": 9,
+      "roomServiceCharges": []
+    },
+    {
+      "id": "5fwrgu4i7k55hl6tb",
+      "userID": 49,
+      "date": "2020/02/06",
+      "roomNumber": 5,
+      "roomServiceCharges": []
+    },
+    {
+      "id": "5fwrgu4i7k55hl6tc",
+      "userID": 22,
+      "date": "2020/01/30",
+      "roomNumber": 13,
+      "roomServiceCharges": []
+    },
+    {
+      "id": "5fwrgu4i7k55hl6td",
+      "userID": 27,
+      "date": "2020/01/31",
+      "roomNumber": 20,
+      "roomServiceCharges": []
+    },
+    {
+      "id": "5fwrgu4i7k55hl6te",
+      "userID": 44,
+      "date": "2020/01/19",
+      "roomNumber": 8,
+      "roomServiceCharges": []
+    },
+    {
+      "id": "5fwrgu4i7k55hl6tf",
+      "userID": 36,
+      "date": "2020/01/25",
+      "roomNumber": 2,
+      "roomServiceCharges": []
+    }
+  ]
+};
+
+
+export { customerSampleData, allCustomerData, allRoomsSampleData, allBookingsSampleData}
 // ADD NEW BOOKING FOR POST SAMPLE
 // {
 //  "userID": 48,

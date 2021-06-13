29e61f9b8267d28c6b6277f1bd87effe7d34946e
Jim Charnesky <mijnesky@hotmail.com>
2021-06-13 11:14:51 -0600

    Add remaining room tests

--- test/room-test.js
+++ test/room-test.js
@@ -51,4 +51,48 @@ describe('Room', () => {
       expect(room2.number).to.equal(11);
     });
 
+    it('should have a room size', () => {
+
+       expect(room.roomType).to.equal('suite');
+     });
+
+    it('should be able to have a different room size', () => {
+
+       expect(room2.roomType).to.equal('single room')
+     });
+
+    it('should tell you if the room has a bidet', () => {
+
+       expect(room.hasBidet).to.equal(false);
+     });
+
+    it('should tell you the size of the beds in a room', () => {
+
+       expect(room2.bedSize).to.equal('twin');
+     });
+
+    it('should tell you the size of the beds in another room', () => {
+
+       expect(room2.bedSize).to.equal('twin');
+     });
+
+    it('should keep track of beds in a room', () => {
+
+       expect(room.numBeds).to.equal(1);
+    });
+
+    it('should keep track of beds in another room', () => {
+
+       expect(room2.numBeds).to.equal(2);
+    });
+
+    it('should have a cost per night', () => {
+
+       expect(room.costPerNight).to.equal(327.24);
+    });
+
+    it('should have a different cost per night', () => {
+
+       expect(room2.costPerNight).to.equal(207.24);
+    });
 });

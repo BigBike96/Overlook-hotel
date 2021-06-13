5b9f6d860f2be2bc653995e855badb117c57fb4a
Jim Charnesky <mijnesky@hotmail.com>
2021-06-13 12:04:43 -0600

    Psudocode hotel class

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,3 +1,13 @@
 // Hotel class
 
+// properties for rooms and bookings
+
+// method to getBookingsByUserId
+// method to get past bookings (use getBookingsByUserId)
+// method to get current bookings
+// method to calculate total lifetime costs of past bookings
+// method to find room details
+// method to find available rooms
+// method to filter rooms by a value
+
 export default Hotel;

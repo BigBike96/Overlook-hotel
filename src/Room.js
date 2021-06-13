65118a9eed90f715e46a9e5536df85bfc8123c12
Jim Charnesky <mijnesky@hotmail.com>
2021-06-13 11:20:50 -0600

    Addcost per night using tdd

--- src/Room.js
+++ src/Room.js
@@ -4,6 +4,8 @@ class Room {
     this.roomType = room.roomType;
     this.hasBidet = room.bidet;
     this.bedSize = room.bedSize;
+    this.numBeds = room.numBeds;
+    this.costPerNight = room.costPerNight;
 
   }
 }
